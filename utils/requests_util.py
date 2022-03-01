d772fae33d9bb732aec118ecebb46dfa6a5cc843
xueyanan <kittynann@163.com>
2022-03-01 19:52:05 +0800

    正常流程的接口测试

--- utils/requests_util.py
+++ utils/requests_util.py
@@ -5,12 +5,13 @@ import requests
 
 
 def requests_get(url, headers=None, data=None):
-    result = requests.get(url, headers=headers ,data=data)
+    result = requests.get(url, headers=headers, data=data)
     code = result.status_code
     try:
         body = result.json()
     except Exception as e:
         body = result.text
+        print(f"{e}不是json")
     contents = dict()
     contents["code"] = code
     contents["body"] = body
